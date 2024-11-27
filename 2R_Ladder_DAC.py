ddd279e3b7968971edf56c307f3b1b615e1c5c76
KyleLorenzCasianan <ckylelorenz@gmail.com>
2024-11-28 07:41:59 +0800

    Removed extra at the end '+' and revised the Bits statement

--- 2R_Ladder_DAC.py
+++ 2R_Ladder_DAC.py
@@ -1,16 +1,21 @@
 import math
 
+VoSum,Vo = 0,0
 VRef = float(input('Enter VRef: '))
 Rf = float(input('Enter Rf: '))
 R = float(input('Enter R: '))
 Digital_input = (input('Enter digital input: '))
-Bits = int(input(f'Enter Bits: '))
-VoSum,Vo = 0,0
+Bits = 1 * len(Digital_input)
 
 print(f'Vo = {-VRef}({Rf}/{R}) (', end = '')
 
 for i in range(Bits):
     VoSum += float(Digital_input[i])/math.pow(2,(i+1))
-    print(f'{Digital_input[i]}/{math.pow(2,(i+1))} + ', end ='')
+    if i == Bits - 1:
+        print(f'{Digital_input[i]}/{math.pow(2, (i + 1))}', end='')
+    else:
+        print(f'{Digital_input[i]}/{math.pow(2, (i + 1))} + ', end='')
+
 Vo = -VRef * ( (Rf/R) * VoSum)
-print(f') = {Vo:.2f}')
+print(f') = {Vo:.2f}V', '\n')
+
